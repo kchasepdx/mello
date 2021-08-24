2e247ad533b9c7015858808127077ed1960772f2
kchasepdx <kchasepdx@gmail.com>
2021-08-24 11:38:32 -0700

    Nav bar edits

--- src/Nav.js
+++ src/Nav.js
@@ -184,7 +184,7 @@ function Nav() {
               </ul>
               {/* USER INFO */}
             </li>
-            {userInfo && userInfo.length > 0 ? (
+            {userInfo ? (
               <div>
                 <li className="nav-item">
                   <p className="nav-link">{userInfo.name}</p>
